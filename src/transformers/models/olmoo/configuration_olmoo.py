5f09d0cae193b3867340344a34f97354a3452cd9
Shane A <shanea@allenai.org>
2024-10-30 16:00:23 -0700

    Fix config errors and converter

--- src/transformers/models/olmoo/configuration_olmoo.py
+++ src/transformers/models/olmoo/configuration_olmoo.py
@@ -95,10 +95,27 @@ class OlmooConfig(PretrainedConfig):
 
     def __init__(
         self,
+        vocab_size=50304,
+        hidden_size=4096,
+        intermediate_size=11008,
+        num_hidden_layers=32,
+        num_attention_heads=32,
+        num_key_value_heads=None,
+        hidden_act="silu",
+        max_position_embeddings=2048,
+        initializer_range=0.02,
+        use_cache=True,
+        pad_token_id=1,
+        bos_token_id=None,
+        eos_token_id=50279,
+        tie_word_embeddings=False,
+        rope_theta=10000.0,
+        rope_scaling=None,
+        attention_bias=False,
+        attention_dropout=0.0,
         rms_norm_eps=1e-6,
         **kwargs,
     ):
-        self.rms_norm_eps = rms_norm_eps
         self.vocab_size = vocab_size
         self.max_position_embeddings = max_position_embeddings
         self.hidden_size = hidden_size
@@ -119,6 +136,7 @@ class OlmooConfig(PretrainedConfig):
         self._rope_scaling_validation()
         self.attention_bias = attention_bias
         self.attention_dropout = attention_dropout
+
         self.rms_norm_eps = rms_norm_eps
 
         super().__init__(
