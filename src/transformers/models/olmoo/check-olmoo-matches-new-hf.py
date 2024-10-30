de01838a28eed80af29f133ba7faec1ea3a8d9b2
Shane A <shanea@allenai.org>
2024-10-30 16:41:42 -0700

    Try move checker to GPU

--- src/transformers/models/olmoo/check-olmoo-matches-new-hf.py
+++ src/transformers/models/olmoo/check-olmoo-matches-new-hf.py
@@ -12,25 +12,27 @@ from transformers import OlmooConfig, OlmooForCausalLM
 def main(olmoo_directory: Path, hf_directory: Path, hf_revision: Optional[str]):
     hf_revision = hf_revision or "main"
 
-    in_vec = torch.randint(0, 50000, (2048, 2))
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    in_vec = torch.randint(0, 50000, (2048, 2), device=device)
 
     print("Loading hf model")
     hf_config = OlmooConfig.from_pretrained(str(hf_directory), revision=hf_revision)
     hf_config._attn_implementation = "sdpa"
     hf_model = OlmooForCausalLM.from_pretrained(
         str(hf_directory), config=hf_config, revision=hf_revision, device_map="auto"
-    ).to(device="cpu")
+    ).to(device=device)
 
     print("Running hf model")
     hf_out = hf_model(in_vec)
 
     print("Loading OLMoo model")
     model_config = ModelConfig.load(str(olmoo_directory / "config.yaml"), key="model")
-    model_config.init_device = "cpu"
-    olmoo_model = olmo.OLMo(model_config)
+    model_config.init_device = str(device)
+    olmoo_model = olmo.OLMo(model_config).to(device)
 
     print("Loading OLMoo state")
-    model_state = torch.load(str(olmoo_directory / "model.pt"), map_location="cpu")
+    model_state = torch.load(str(olmoo_directory / "model.pt"), map_location=str(device))
     model_state = {key.removeprefix("model."): val for key, val in model_state.items()}
 
     print("Loading state into OLMoo")
