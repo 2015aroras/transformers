Fix precision mismatches and use sigmoid for moe router gate

src/transformers/models/olmoe2/modeling_olmoe2.py
@@ -42,7 +42,7 @@ class Olmoe2RMSNorm(nn.Module):
         hidden_states = hidden_states.to(torch.float32)
         variance = hidden_states.pow(2).mean(-1, keepdim=True)
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
+        return (self.weight * hidden_states).to(input_dtype)
 
     def extra_repr(self):
         return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
@@ -75,11 +75,12 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
     Returns:
         `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
     """
+    q_type, k_type = q.dtype, k.dtype
     cos = cos.unsqueeze(unsqueeze_dim)
     sin = sin.unsqueeze(unsqueeze_dim)
     q_embed = (q * cos) + (rotate_half(q) * sin)
     k_embed = (k * cos) + (rotate_half(k) * sin)
-    return q_embed, k_embed
+    return q_embed.to(q_type), k_embed.to(k_type)
 
 
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
@@ -271,7 +272,8 @@ class Olmoe2SparseMoeBlock(nn.Module):
         # router_logits: (batch * sequence_length, n_experts)
         router_logits = self.gate(hidden_states)
 
-        routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
+        # routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
+        routing_weights = F.sigmoid(router_logits.float())
         routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
         if self.norm_topk_prob:
             routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
@@ -424,8 +426,7 @@ class Olmoe2RotaryEmbedding(nn.Module):
             emb = torch.cat((freqs, freqs), dim=-1)
             cos = emb.cos() * self.attention_scaling
             sin = emb.sin() * self.attention_scaling
-
-        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
+            return cos, sin
 
 
 @auto_docstring
@@ -954,4 +955,4 @@ class Olmoe2ForCausalLM(Olmoe2PreTrainedModel, GenerationMixin):
         )
 
 
-__all__ = ["Olmoe2ForCausalLM", "Olmoe2Model", "Olmoe2PreTrainedModel"]
+__all__ = ["Olmoe2ForCausalLM", "Olmoe2Model", "Olmoe2RotaryEmbedding", "Olmoe2PreTrainedModel"]

src/transformers/models/olmoe2/modular_olmoe2.py
@@ -8,7 +8,8 @@ from transformers.modeling_utils import ALL_ATTENTION_FUNCTIONS
 from ...cache_utils import Cache
 from ...pytorch_utils import ALL_LAYERNORM_LAYERS
 from ...utils import logging
-from ..llama.modeling_llama import LlamaRMSNorm, eager_attention_forward
+from ..llama.modeling_llama import eager_attention_forward
+from ..olmo2.modeling_olmo2 import Olmo2RMSNorm, Olmo2RotaryEmbedding, apply_rotary_pos_emb
 from ..olmoe.configuration_olmoe import OlmoeConfig
 from ..olmoe.modeling_olmoe import (
     OlmoeAttention,
@@ -17,7 +18,6 @@ from ..olmoe.modeling_olmoe import (
     OlmoeMLP,
     OlmoeModel,
     OlmoeSparseMoeBlock,
-    apply_rotary_pos_emb,
 )
 
 
@@ -186,7 +186,7 @@ class Olmoe2Config(OlmoeConfig):
 #     pass
 
 
-class Olmoe2RMSNorm(LlamaRMSNorm):
+class Olmoe2RMSNorm(Olmo2RMSNorm):
     pass
 
 
@@ -283,7 +283,8 @@ class Olmoe2SparseMoeBlock(OlmoeSparseMoeBlock):
         # router_logits: (batch * sequence_length, n_experts)
         router_logits = self.gate(hidden_states)
 
-        routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
+        # routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
+        routing_weights = F.sigmoid(router_logits.float())
         routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
         if self.norm_topk_prob:
             routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
@@ -379,6 +380,10 @@ class Olmoe2DecoderLayer(OlmoeDecoderLayer):
         return outputs
 
 
+class Olmoe2RotaryEmbedding(Olmo2RotaryEmbedding):
+    pass
+
+
 class Olmoe2Model(OlmoeModel):
     pass
 
@@ -391,5 +396,6 @@ __all__ = [
     "Olmoe2Config",
     "Olmoe2ForCausalLM",
     "Olmoe2Model",
+    "Olmoe2RotaryEmbedding",
     "Olmoe2PreTrainedModel",  # noqa: F822
 ]